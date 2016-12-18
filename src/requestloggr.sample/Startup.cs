5f5c98886a6f817e43f42cf8e772d8e686e92f50
Anders Asperheim <anders@asperheim.no>
2016-12-18 02:28:53 +0100

    Add project files.

---
+++ src/requestloggr.sample/Startup.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using Serilog;
+using ILogger = Microsoft.Extensions.Logging.ILogger;
+
+namespace requestloggr.sample
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+            loggerFactory.AddProvider(new RequestLoggrProvider());
+            loggerFactory.UseRequestLoggr();
+
+            //app.UseRequestLoggr();
+            app.UseMvc();
+
+            
+        }
+    }
+
+    public static class RequestLoggrLoggerFactory
+    {
+        public static ILoggerFactory UseRequestLoggr(this ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddProvider(new RequestLoggrProvider());
+
+            return loggerFactory;
+        }
+
+    }
+
+    public class RequestLoggrLogger : ILogger, IDisposable
+    {
+        private bool[] _enabledLogLevels;
+        public RequestLoggrLogger()
+        {
+            _enabledLogLevels = new bool[Enum.GetNames(typeof(LogLevel)).Length];
+
+            _enabledLogLevels[(int) LogLevel.Debug] = true;
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return _enabledLogLevels[logLevel.GetHashCode()];
+        }
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            // Uh oh..?
+            return this;
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            Debug.WriteLine("Dette kommer fra awesomeness!!");
+        }
+
+        public void Dispose()
+        {
+            //Le disposé must (h)append (h)ere
+        }
+    }
+    public class RequestLoggrProvider : ILoggerProvider, IDisposable
+    {
+        public static ILoggerProvider RequestLoggr()
+        {
+            return new RequestLoggrProvider();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new RequestLoggrLogger();
+        }
+
+
+    }
+
+    public class RequestLoggerMiddlewareDelegate
+    {
+        private readonly RequestDelegate _next;
+
+        private readonly ILogger _logger;
+
+        public RequestLoggerMiddlewareDelegate(RequestDelegate next, ILogger logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            
+            Debug.WriteLine("Before the execution!");
+            await _next.Invoke(context);
+            Debug.WriteLine("After the execution!");
+
+            Debug.WriteLine("Statuscode: " + context.Response.StatusCode);
+            Debug.WriteLine("Executiontime: " + context.Response.StatusCode);
+        }
+    }
+
+    public static class RequestLogger
+    {
+        public static IApplicationBuilder UseRequestLoggr(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<RequestLoggerMiddlewareDelegate>();
+        }
+    }
+}
